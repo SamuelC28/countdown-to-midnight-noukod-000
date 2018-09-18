780dd00d652e1d4a3c9f1783c23df218e585ff48
Samuel Cornet <samuel@noukod.com>
2018-09-18 18:42:34 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
 
-def countdown
+
+def countdown_with_sleep(number) 
+  number = sleep(5)   
+end 
+
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+     number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+
+
